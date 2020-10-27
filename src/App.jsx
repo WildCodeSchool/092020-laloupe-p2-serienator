a742d4488e07cbfbae9123d0bd420164f7bc4403
Thibaut <thibaut.guibert@edu.escp.eu>
2020-10-27 12:05:26 +0100

    Modified metadata from index.html and json, added fonts

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,7 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  const value = 0;
-  return (
-    <main className="container">
-      <div className="item">
-        <h1>Hello world !</h1>
-      </div>
-      <div className="item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="item">
-        <p>
-          Start in the <code>App.jsx</code> component ! {value}
-        </p>
-      </div>
-    </main>
-  );
+  return <main className="container" />;
 }
 
 export default App;
