67553d7dcad0bab13f50d93057c6b8cc25ea967f
ingrid-schwarz <ingrid.schwarz.wcs@gmail.com>
2020-10-27 16:30:56 +0100

    create matchmaking and cards-mobile components

---
+++ src/components/CardsMobile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Cards_mobile.css';
+function CardsMobile(){
+    return(
+        <button className="cardsMobile">   
+                <h1>coucou!</h1>                               
+                <img src="" alt="Sélectionner un série"/>
+        </button>
+    );
+}
+export default CardsMobile;
