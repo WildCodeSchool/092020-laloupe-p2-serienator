eef355b4f31e267ffea72c0c540190aff1f8975f
Damien Mauger <mauger.damien@hotmail.fr>
2020-10-28 12:06:42 +0100

    creation of componant for card display

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,12 @@
 import React from "react";
+import NosReco from "./components/NosReco";
 
 import "./App.css";
 
 function App() {
-  const value = 0;
   return (
     <main className="container">
-      <div className="item">
-        <h1>Hello world !</h1>
-      </div>
-      <div className="item">
-        <p>Welcome to your fresh, lightweight, React App ! &#127752;</p>
-      </div>
-      <div className="item">
-        <p>
-          Start in the <code>App.jsx</code> component ! {value}
-        </p>
-      </div>
+      <NosReco />
     </main>
   );
 }
---
+++ src/components/NosReco.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+
+function NosReco() {
+  return (
+    <section>
+      <div>
+        <h2>Voici nos recommandations</h2>
+        <p>Les avez-vous déjà vues ?</p>
+      </div>
+      <div>
+        <div>
+          <img
+            src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
+            alt="Casa de Papel"
+          />
+          <p>Casa de papel</p>
+          <p>(2017)</p>
+        </div>
+        <div>
+          <img
+            src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
+            alt="Casa de Papel"
+          />
+          <p>Casa de papel</p>
+          <p>(2017)</p>
+        </div>
+        <div>
+          <img
+            src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
+            alt="Casa de Papel"
+          />
+          <p>Casa de papel</p>
+          <p>(2017)</p>
+        </div>
+        <div>
+          <img
+            src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
+            alt="Casa de Papel"
+          />
+          <p>Casa de papel</p>
+          <p>(2017)</p>
+        </div>
+        <div>
+          <img
+            src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
+            alt="Casa de Papel"
+          />
+          <p>Casa de papel</p>
+          <p>(2017)</p>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default NosReco;
