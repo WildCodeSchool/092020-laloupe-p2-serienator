641afa2e6cedb1ffffaa3d77376ced51e8e931ea
Damien Mauger <mauger.damien@hotmail.fr>
2020-10-28 14:59:26 +0100

    modification of CSS

--- src/components/NosReco.jsx
+++ src/components/NosReco.jsx
@@ -1,52 +1,53 @@
 import React from "react";
+import "./NosReco.css";
 
 function NosReco() {
   return (
-    <section>
+    <section className="display-nos-reco">
       <div>
         <h2>Voici nos recommandations</h2>
-        <p>Les avez-vous déjà vues ?</p>
+        <p className="subtitle-nos-reco">Les avez-vous déjà vues ?</p>
       </div>
-      <div>
-        <div>
+      <div className="container-nos-reco">
+        <div className="cards-nos-reco">
           <img
             src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
             alt="Casa de Papel"
           />
-          <p>Casa de papel</p>
-          <p>(2017)</p>
+          <h4>Casa de papel</h4>
+          <p className="year-nos-reco">(2017)</p>
         </div>
-        <div>
+        <div className="cards-nos-reco">
           <img
             src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
             alt="Casa de Papel"
           />
-          <p>Casa de papel</p>
-          <p>(2017)</p>
+          <h4>Casa de papel</h4>
+          <p className="year-nos-reco">(2017)</p>
         </div>
-        <div>
+        <div className="cards-nos-reco">
           <img
             src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
             alt="Casa de Papel"
           />
-          <p>Casa de papel</p>
-          <p>(2017)</p>
+          <h4>Casa de papel</h4>
+          <p className="year-nos-reco">(2017)</p>
         </div>
-        <div>
+        <div className="cards-nos-reco">
           <img
             src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
             alt="Casa de Papel"
           />
-          <p>Casa de papel</p>
-          <p>(2017)</p>
+          <h4>Casa de papel</h4>
+          <p className="year-nos-reco">(2017)</p>
         </div>
-        <div>
+        <div className="cards-nos-reco">
           <img
             src="https://image.tmdb.org/t/p/w200/eDlCaXdC5Z8elYuRto7pPNDb3ho.jpg"
             alt="Casa de Papel"
           />
-          <p>Casa de papel</p>
-          <p>(2017)</p>
+          <h4>Casa de papel</h4>
+          <p className="year-nos-reco">(2017)</p>
         </div>
       </div>
     </section>
