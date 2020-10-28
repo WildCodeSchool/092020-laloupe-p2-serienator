01fc860c408f1cbf2ddf5da3ffed76bf239dc1b8
ingrid-schwarz <ingrid.schwarz.wcs@gmail.com>
2020-10-28 15:22:59 +0100

    function display for searchbar, prompt used for test

--- src/components/CardsMobile.jsx
+++ src/components/CardsMobile.jsx
@@ -2,13 +2,29 @@ import React from "react";
 import "./CardsMobile.css";
 import questionMark from "../images/question-mark.png";
 
-function CardsMobile() {
-  return (
-    <div className="cards-mobile">
-      <button className="qmark" type="submit">
-        <img className="qmark" src={questionMark} alt="Sélectionner un série" />
-      </button>
-    </div>
-  );
+class CardsMobile extends React.Component {
+  constructor() {
+    super();
+    this.displayPrompt = this.displayPrompt.bind(this);
+  }
+  //     displayPrompt () {
+  //         let serie = prompt("Entrez le nom d'une série");
+  //         console.log(serie);
+  // }
+
+  render() {
+    return (
+      <div className="cards-mobile">
+        <button className="qmark" type="button" onClick={this.displayPrompt}>
+          <img
+            className="qmark"
+            src={questionMark}
+            alt="Sélectionner un série"
+          />
+        </button>
+      </div>
+    );
+  }
 }
+
 export default CardsMobile;
