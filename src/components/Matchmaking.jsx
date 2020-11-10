43ad9364f65e83da336d80358f502e5f9a100205
Thibaut <thibaut.guibert@edu.escp.eu>
2020-11-10 17:17:43 +0100

    removed state from TvDisplay to bring it to parent Matchmaking

--- src/components/Matchmaking.jsx
+++ src/components/Matchmaking.jsx
@@ -3,6 +3,12 @@ import "./matchmaking.css";
 import imgDefault from "../images/questioncard3.jpeg";
 import MatchmakingCard from "./MatchmakingCard";
 import TvDisplay from "./TvDisplay";
+import loadingScreen from "../images/loading-screen.gif";
+import offScreen from "../images/screen-tv-off.jpg";
+import glitchScreen from "../images/screen-tv-glitch800.webp";
+import successScreen from "../images/success.gif";
+
+const screenStep = [offScreen, glitchScreen, loadingScreen, successScreen];
 
 class Matchmaking extends React.Component {
   constructor() {
@@ -16,17 +22,32 @@ class Matchmaking extends React.Component {
         name: "question mark card",
         poster_path: imgDefault,
       },
+      screen: offScreen,
+      counter: 1,
     };
+    this.handleScreen = this.handleScreen.bind(this);
+  }
+
+  handleScreen() {
+    const { counter } = this.state;
+    if (counter < 3) {
+      const newcounter = counter + 1;
+      this.setState({ counter: newcounter });
+    } else {
+      const newcounter = 0;
+      this.setState({ counter: newcounter });
+    }
+    this.setState({ screen: screenStep[counter] });
   }
 
   render() {
-    const { firstSeries, secondSeries } = this.state;
+    const { firstSeries, secondSeries, screen } = this.state;
     return (
       <section className="matchmaking">
         <h2>Quelles sont les 2 dernières séries que tu as regardées ?</h2>
         <div className="matchmaking-container">
           <MatchmakingCard card={firstSeries} />
-          <TvDisplay />
+          <TvDisplay screen={screen} handleScreen={this.handleScreen} />
           <MatchmakingCard card={secondSeries} />
         </div>
       </section>
