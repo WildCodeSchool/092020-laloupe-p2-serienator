695b0083ee62a589a4cecc9c78f3d7a2d4bdbff9
toto <torphi78@gmail.com>
2020-11-25 17:45:38 +0100

    add mobile component for top3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import OurReco from "./components/OurReco";
 import Lucky from "./components/Lucky";
 import Footer from "./components/Footer";
 import TransitionTop3 from "./components/TransitionTop3";
-import Slide from "./components/Slide";
+// import Slide from "./components/Slide";
 import "./App.css";
 import imgDefault from "./images/questioncard3.jpeg";
 import loadingScreen from "./images/loading-screen.gif";
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -3,18 +3,38 @@ import "./Slide.css"
 
 function Slide() {
     return (
-        <div>
+        <div className="accordeon">
             <div class="accordion">
-     <input type="radio" name="select" class="accordion-select" checked />
-    <div class="accordion-title"><span>Title</span></div>
-    <div class="accordion-content">Content</div> 
-         <input type="radio" name="select" class="accordion-select" />
-    <div class="accordion-title"><span>Title</span></div>
-    <div class="accordion-content">Content</div> 
-         <input type="radio" name="select" class="accordion-select" />
-    <div class="accordion-title"><span>Title</span></div>
-    <div class="accordion-content">Content</div> 
-</div> 
+
+                        {/* <div className="titreTop3"> */}
+                                <input type="radio" name="select" class="accordion-select" checked />
+                            <div class="accordion-title"><span>Top 3 Séries Netflix selon Ingrid </span></div>
+                            <div class="accordion-content">
+                            {/* {isShowedIngrid ? <Top3Ingrid /> : ""} */}
+                            
+                            </div> 
+                        {/* </div> */}
+
+                        {/* <div className="titreTop3"> */}
+                                <input type="radio" name="select" class="accordion-select" />
+                            <div class="accordion-title"><span>Top 3 Séries Netflix selon Philippe </span></div>
+                            <div class="accordion-content">Content</div> 
+                        {/* </div> */}
+                        
+
+                        {/* <div className="titreTop3"> */}
+                                <input type="radio" name="select" class="accordion-select" />
+                            <div class="accordion-title"><span>Top 3 Séries Netflix selon Thibaut</span></div>
+                            <div class="accordion-content">Content</div>
+                        {/* </div> */}
+
+
+                        {/* <div className="titreTop3"> */}
+                                <input type="radio" name="select" class="accordion-select" />
+                            <div class="accordion-title"><span>Top 3 Séries Netflix selon Damien</span></div>
+                            <div class="accordion-content">Content</div>
+                        {/* </div> */}
+            </div> 
         </div>
     )
 }
